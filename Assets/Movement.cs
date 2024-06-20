d3f8363aaf7a1910c37b13f42434e29019b56e19
www <mkozlov@edu.cdv.pl>
2024-06-20 19:41:35 +0200

    work2

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,46 +1,51 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
 
-
-
     public SpriteRenderer sprite;
     public Rigidbody2D rigid;
+   
     
-    
-    
-    // Start is called before the first frame update
+    public float speed = 10f;
+    public KeyCode Upkey = KeyCode.W;
+    public KeyCode Downkey = KeyCode.S;
+
+   
+
+
+    // Start is called before the first frame update 
     void Start()
     {
-        print("Hello from the start");
-        sprite = GetComponent<SpriteRenderer>();
         rigid = GetComponent<Rigidbody2D>();
-
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(Upkey) && transform.position.y < 4.5f  )
         {
-            rigid.velocity = Vector2.up;
+            rigid.velocity = Vector2.up * speed;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(Downkey) && transform.position.y > -4.5f )
         {
-            rigid.velocity = Vector2.down;
+            rigid.velocity = Vector2.down * speed;
         }
         else
         {
-            rigid.velocity = Vector2.zero;
+            rigid.velocity = Vector2.zero;      
         }
     }
 
     private void OnMouseDown()
     {
+
         sprite.color = Color.red;
+        rigid = GetComponent<Rigidbody2D>();
     }
 }
